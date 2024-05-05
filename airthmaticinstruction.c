2d525e2fa0bd916ce2cf96d113cad97ba8370d62
“[priyanshu <priyanshu9572596395@gmail.com>
2024-05-05 20:15:25 +0530

    add arithmetic

---
+++ airthmaticinstruction.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>//power wala print karne ke liye math.h ka use karte hai
+int main()
+{
+    int a = 4;
+    int b = 8;
+    printf("the value of a + b is: %d",a + b);
+    printf("the value of a - b is: %d",a - b);
+    printf("the value of a * b is: %d",a * b);
+    printf("the value of a / b is: %d",a / b);
+    printf("the value of 2 to the power of 5 is %f \n",pow(2,5));
+    return 0;
+}
